f6e219ceffd07691c22fb51b25668b7ae6055654
Damian Montero <photoca@hotmail.com>
2015-12-02 18:30:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Node Websocket Exmple
+
+A tiny demo of the simple websocket example using NODE for the backend
+
+# Running Locally
+
+``` close
+git clone https://github.com/thedamian/websocket-node-example
+npm install ws
+node index.js
+(Then browse to http://localhost:5000)
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+var RunMessage = true;
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    if (RunMessage) {
+      ws.send(JSON.stringify(new Date()), function() {  });
+    }
+  }, 1000)
+
+  ws.on('message', function incoming(message) {
+    //console.log('Websocket Message: %s', message);
+    switch(message) {
+      case "start": RunMessage = true; 
+                    break;
+      case "stop":  RunMessage = false; 
+                    break;
+    }
+});
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
