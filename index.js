cd0024911c49e319bb142d40da655eaeb41c3fe5
Damian Montero <damian@gruups.com>
2015-12-02 17:50:43 -0500

    do it

--- index.js
+++ index.js
@@ -10,14 +10,25 @@ var server = http.createServer(app)
 server.listen(port)
 
 console.log("http server listening on %d", port)
-
+var RunMessage = true;
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
+    if (RunMessage) {
+      ws.send(JSON.stringify(new Date()), function() {  });
+    }
   }, 1000)
+
+wss.on("message",function incoming(message) {
+  switch(message) {
+    case "start": RunMessage = true; 
+                  break;
+    case "stop":  RunMessage = false; 
+                  break;
+  }
+});
 
   console.log("websocket connection open")
 
